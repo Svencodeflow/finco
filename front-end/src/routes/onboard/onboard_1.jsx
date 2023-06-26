4fe6b4746f4644b0259d4e406e5adef85bf52708
Sven Schlaak <123948019+Svencodeflow@users.noreply.github.com>
2023-06-26 15:50:06 +0200

    a

--- front-end/src/routes/onboard/onboard_1.jsx
+++ front-end/src/routes/onboard/onboard_1.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "../../style/onboard.css";
-
-import bankcard from "./../../images/bankcard.png";
+import bankcard from "./../../images/onboard_bank.png";
 
 export default function onboard_1() {
     return (
