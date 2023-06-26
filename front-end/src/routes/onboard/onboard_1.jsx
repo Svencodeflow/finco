fa64c6803274d83a9e14026776d88250b26aa2a7
Sven Schlaak <123948019+Svencodeflow@users.noreply.github.com>
2023-06-26 15:27:53 +0200

    Update onboard_1.jsx

--- front-end/src/routes/onboard/onboard_1.jsx
+++ front-end/src/routes/onboard/onboard_1.jsx
@@ -5,7 +5,9 @@ import bankcard from "../../images/onboard_bank.png";
 export default function onboard_1() {
     return (
         <div id="onboard_1">
-            <img src={bankcard} alt="bankcard" />
+            <div className="onboard_1_img">
+                <img src={bankcard} alt="bankcard" />
+            </div>
         </div>
     );
 }
