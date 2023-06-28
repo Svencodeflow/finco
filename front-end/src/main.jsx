6289164dc67c9a2b17382e756abb3556561cd261
Sven Schlaak <123948019+Svencodeflow@users.noreply.github.com>
2023-06-28 11:59:18 +0200

    login v0.1

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -8,6 +8,7 @@ import Splash from "./routes/onboard/splash";
 import Onboard1 from "./routes/onboard/Onboard1.jsx";
 import Onboard2 from "./routes/onboard/Onboard2.jsx";
 import Register from "./routes/login/Register";
+import Login from "./routes/login/Login";
 import Accset from "./routes/login/Accset";
 
 const router = createBrowserRouter([
@@ -34,6 +35,11 @@ const router = createBrowserRouter([
     {
         path: "/register",
         element: <Register />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
         errorElement: <ErrorPage />,
     },
     {
--- front-end/src/routes/login/Login.jsx
+++ front-end/src/routes/login/Login.jsx
@@ -1,7 +1,59 @@
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import Logo from "../../images/Logo.svg";
+import "../../style/login.css";
+
 export default function Login() {
+    const navigate = useNavigate(); // get the navigate function
+    const location = useLocation(); // get the location object
+    const handleClick = () => {
+        navigate("/"); // navigate to the relative path
+        console.log(location.pathname); // log the current path
+    };
+
     return (
-        <div>
-            <h1>Login</h1>
+        <div id="Login">
+            <div className="Login_logo">
+                <img src={Logo} alt="logo" />
+            </div>
+            <div className="Login_text">
+                <h2>Welcome Back</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adip sicing elit,
+                    sed do eiusmod.
+                </p>
+            </div>
+            <div className="Login_form">
+                <form>
+                    <div className="Login_form_input">
+                        <div>
+                            <input
+                                type="text"
+                                id="name"
+                                required
+                                placeholder="Name"
+                            />
+                        </div>
+                        <div>
+                            <input
+                                type="email"
+                                id="email"
+                                required
+                                placeholder="Email"
+                            />
+                        </div>
+                    </div>
+                    <div className="Login_form_btn">
+                        <button type="submit" onClick={handleClick}>
+                            Login
+                        </button>
+                    </div>
+                </form>
+            </div>
+            <div className="Login_text">
+                <p>
+                    Don’t have any account? <Link to={"/accset"}>Sign up</Link>
+                </p>
+            </div>
         </div>
     );
 }
