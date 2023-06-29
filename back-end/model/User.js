ab9c8f87cf3fec74ae5e5280a338c0a68c000d8c
WebDevManuel <donmanuel2005@web.de>
2023-06-29 12:37:17 +0200

    Customize(User.js): define userSchema

--- back-end/model/User.js
+++ back-end/model/User.js
@@ -1,31 +1,9 @@
 import mongoose from "mongoose";
-import crypto from "crypto";
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true, lowercase: true },
-  salt: { type: String, required: true, select: false },
-  hash: { type: String, required: true, select: false },
-  posts: [{ type: mongoose.SchemaTypes.ObjectId, ref: "Post" }],
+  password: { type: String, require: true },
 });
-
-// Nur `function` Funktionen benutzen nicht
-// Arrow (()=>) functions
-userSchema.methods.setPassword = function (password) {
-  // Salt erstellen
-  this.salt = crypto.randomBytes(64).toString("hex");
-  // Password mit salt hashen
-  this.hash = crypto
-    .pbkdf2Sync(password, this.salt, 1000, 64, "sha512")
-    .toString("hex");
-};
-
-userSchema.methods.verifyPassword = function (password) {
-  const hash = crypto
-    .pbkdf2Sync(password, this.salt, 1000, 64, "sha512")
-    .toString("hex");
-
-  return this.hash === hash;
-};
 
 export const User = mongoose.model("User", userSchema);
