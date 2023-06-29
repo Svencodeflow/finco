ac3007ec95e90d87a1287fa7ee9197c256a9face
WebDevManuel <donmanuel2005@web.de>
2023-06-29 16:37:37 +0200

    Customize(server.js, User.js): add signup

--- back-end/model/User.js
+++ back-end/model/User.js
@@ -1,9 +1,21 @@
 import mongoose from "mongoose";
+import crypto from "crypto";
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true, lowercase: true },
-  password: { type: String, require: true },
+  salt: { type: String, required: true, select: false },
+  hash: { type: String, required: true, select: false },
 });
+
+// set password | only use function expressions instead of arrow functions
+userSchema.methods.setPassword = function (password) {
+  // Salt erstellen
+  this.salt = crypto.randomBytes(64).toString("hex");
+  // Password mit salt hashen
+  this.hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, "sha512")
+    .toString("hex");
+};
 
 export const User = mongoose.model("User", userSchema);
--- back-end/server.js
+++ back-end/server.js
@@ -1,29 +1,34 @@
 import express from "express";
 import dotenv from "dotenv";
 import { User } from "./model/index.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config({ path: new URL("../.env", import.meta.url).pathname });
 
 const PORT = process.env.BE_PORT || 3000;
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); //jasonParser to a js object
+app.use(cookieParser());
 
 app.get("/api/status", (req, res) => {
   res.send({ status: "Ok" });
 });
 
+//--------------SIGNUP--------------\\
 app.post("/api/signup", async (req, res) => {
-  // Neuen User erstellen
-  const { name, email, password } = req.body;
-  const newUser = new User({ name, email, password });
-
+  // create new user
+  const { name, email } = req.body;
+  const newUser = new User({ name, email });
+  // user.setPassword (set hash and salt)
+  newUser.setPassword(req.body.password);
+  // save user
   try {
     await newUser.save();
     return res.send({
       data: {
         message: "New user created",
-        user: { name, email, password },
+        user: { name, email },
       },
     });
   } catch (e) {
@@ -32,7 +37,7 @@ app.post("/api/signup", async (req, res) => {
       return res.status(400).send({ error: e });
     };
 
-    // Duplication Error email existiert bereits als user
+    // duplication Error -> email exist already
     if (e.name === "MongoServerError" && e.code === 11000) {
       console.log("Account exists already");
       return res.status(400).send({
