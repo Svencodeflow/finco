a354cf851fe231b1ec8479a64f19fd412128e547
KevOnTheRockz <123948086+KevinKasperek@users.noreply.github.com>
2023-07-05 07:47:01 +0200

    KK

--- front-end/src/routes/transaction/onlysearch.jsx
+++ front-end/src/routes/transaction/onlysearch.jsx
@@ -1,29 +1,48 @@
-import React from 'react';
-import { Container, Input, Button, Icon } from 'semantic-ui-react';
-import 'semantic-ui-css/semantic.min.css';
-import './style/transaction.css';
+import React from "react";
+
+import {
+    Image,
+} from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import "../../style/transactiontotal.css";
+import Logo from "../../images/Logo.svg";
+import profil_logo from "../../images/profil_logo.svg";
+import Date from "../../images/date.png";
+import Search from "../../images/Search.png";
 
 
 
 function onlysearch() {
   return (
-    <>
+      <>
           <div>
-          <Link to="/">
-              <img src={home} alt="home" />
-          </Link>
-          <Link to="/addtransaction">
-            <img src={alltransact} alt="alltransact" />
-          </Link>
+              <div className="header">
+                  <aside>
+                      <Image src={Logo} alt="Logo" />
+                  </aside>
+                  <section>
+                      <Image src={profil_logo} alt="profil_logo" />
+                  </section>
+              </div>
 
-        <Container className="search-container">
-          <Input icon="search" placeholder="Search..." />
-          <Button icon labelPosition="left">
-            <Icon name="calendar" />
-          </Button>
-        </Container>
-      </div>
-    </>
+              <main>
+               <div>
+                      <div className="allbutton">
+                          <h1> All Transaction</h1>
+                          <div className="button">
+                          <button className="seacabutton">
+                          <img src={Date} alt="Date" />
+                          </button>
+                          <button className="seacabutton">
+                          <img src={Search} alt="Search" />
+                          </button>
+                          </div>
+              </div> 
+
+                      </div>
+              </main>
+          </div>
+      </>
   );
 }
 
