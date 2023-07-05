58726a4158eb90ad73ab2f433edc110a7fac5ae0
KevOnTheRockz <123948086+KevinKasperek@users.noreply.github.com>
2023-07-05 10:33:47 +0200

    update

--- front-end/src/routes/transaction/onlysearch.jsx
+++ front-end/src/routes/transaction/onlysearch.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 import {
-    Image,
+    Image, Input
 } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import "../../style/transactiontotal.css";
+import "../../style/addincome.css";
 import Logo from "../../images/Logo.svg";
 import profil_logo from "../../images/profil_logo.svg";
 import Date from "../../images/date.png";
@@ -13,35 +14,35 @@ import Search from "../../images/Search.png";
 
 
 function onlysearch() {
-  return (
-      <>
-          <div>
-              <div className="header">
-                  <aside>
-                      <Image src={Logo} alt="Logo" />
-                  </aside>
-                  <section>
-                      <Image src={profil_logo} alt="profil_logo" />
-                  </section>
-              </div>
+    return (
+        <>
+        <div>
+            <header id="income_header">
+                <div className="income_back">
+                    <img src={Logo} alt="Logo" />
+                </div>
+                <div className="income_profil">
+                    <img src={profil_logo} alt="profil_logo" />
+                </div>
+            </header>
 
-              <main>
-               <div>
-                      <div className="allbutton">
-                          <h1> All Transaction</h1>
-                          <div className="button">
-                          <button className="seacabutton">
-                          <img src={Date} alt="Date" />
-                          </button>
-                          <button className="seacabutton">
-                          <img src={Search} alt="Search" />
-                          </button>
-                          </div>
-              </div> 
+            <main>
+                <div>
+                    <div className="allbutton">
+                        <h1> S</h1>
+                        <div className="button">
+                            <button className="seacabutton">
+                                <img src={Date} alt="Date" />
+                            </button>
+                            <div className="search-bar">
+                                <Input icon={<img src={Search} alt="Search" />} placeholder="Search..." />
+                            </div>
+                        </div>
+                    </div>
 
-                      </div>
-              </main>
-          </div>
+                </div>
+            </main>
+        </div >
       </>
   );
 }
