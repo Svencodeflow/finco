a5b048694e50ddedf1f5d7f4c916de37ece3813c
Sven Schlaak <123948019+Svencodeflow@users.noreply.github.com>
2023-07-08 23:27:04 +0200

    Update vite.config.js

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
 
 // https://vitejs.dev/config/
 // 1. We are importing the defineConfig function from the Vite library. 2. We are calling the defineConfig function and passing an object to it that has two properties, plugins and server. 3. The plugins property is an array containing the react plugin that we imported from the @vitejs/plugin-react-refresh library. 4. The server property is also an object and contains the port and proxy properties. 5. The port property is set to 3500. This means that we are telling the Vite server to listen on port 3500 for incoming requests. 6. The proxy property is set to an object that contains the /api property. This means that any request that starts with /api will be proxied to the target http://localhost:3000/api. This is useful because we don't want to have to specify the full url when making requests to our backend API. 
@@ -13,7 +15,8 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['mongoose', 'crypto-js'],
+      external: [path.resolve(__dirname, 'node_modules/mongoose'),
+      'crypto-js',],
     },
   },
 })
