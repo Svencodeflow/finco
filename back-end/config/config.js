59f5f2eb1d23bcac87d4b5b8b293304a86beb32f
Sven Schlaak <123948019+Svencodeflow@users.noreply.github.com>
2023-07-09 19:56:55 +0200

    Update config.js

--- back-end/config/config.js
+++ back-end/config/config.js
@@ -2,9 +2,11 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 
-dotenv.config({ path: fileURLToPath(new URL("../../.env", import.meta.url)) });
+dotenv.config({
+    path: fileURLToPath(new URL("/etc/secrets/.env", import.meta.url)),
+});
 
-console.log("process.env.PATHNAME", fileURLToPath(new URL("../../.env", import.meta.url)));
+// console.log("process.env.PATHNAME", fileURLToPath(new URL("../../.env", import.meta.url)));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
